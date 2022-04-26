7430f2c547546fc72333437c35122b09dda366bb
nataliakif <nataliakif@gmail.com>
2022-04-26 12:45:13 +0300

    finished

--- src/getPhotos.js
+++ src/getPhotos.js
@@ -1,17 +1,34 @@
 import axios from 'axios';
-
-    async function getPhotos(query) {
-        const response = await axios.get('https://pixabay.com/api/', {
-        params: {
-            key: '26861393-22f68a93d11eca8c337dbb001',
-            q: `${query}`,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: true,
-            page: 1,
-            per_page: 40,
-          }
-        });
-        return response;
+export default class ImagesApiService{
+  constructor(){
+    this.options ={
+      params: {
+        key: '26861393-22f68a93d11eca8c337dbb001',
+        q: '',
+        orientation: 'horizontal',
+        image_type: 'photo',
+        safesearch: true,
+        page: 1,
+        per_page: 40,
       }
-export default{getPhotos}
+  }}
+  get query(){
+    return this.options.params.q;
+  }
+  set query(newQuery){
+    return this.options.params.q = newQuery;
+  }
+  resetPage(){
+    this.options.params.page = 1;
+  }
+  getPage(){
+    return this.options.params.page;
+  }
+
+  async getImages() {
+  const response = await axios.get('https://pixabay.com/api/', this.options);
+  this.options.params.page += 1;
+  return response;
+} 
+}
+   
--- src/index.js
+++ src/index.js
@@ -3,39 +3,58 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 // Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
-import API from './getPhotos'
+import ImagesApiService from './getPhotos'
 
 const form = document.querySelector('.search-form');
 const input = document.querySelector('[name="searchQuery"]');
 const searchBtn = document.querySelector('[type="submit"]');
 const gallery = document.querySelector('.gallery');
+const loadMoreBtn =document.querySelector('.load-more');
 
   let galleryLightBox = new SimpleLightbox('.gallery a');
   
 form.addEventListener('submit', onSearchButton);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
+const imagesApiService = new ImagesApiService();
+loadMoreBtn.disabled =true;
 function onSearchButton(event){
     event.preventDefault();
-    const formElement = event.currentTarget.elements;
-    const query = formElement.searchQuery.value;
-   if(query){
-   API.getPhotos(query)
-   .then(dataProcessing)
+    const query =event.currentTarget.elements.searchQuery.value;
+    imagesApiService.query =query;
+    imagesApiService.resetPage();
+    gallery.innerHTML = '';
+    imagesApiService.getImages()
+    .then(dataProcessing)
    .catch(error=>{
        console.log(error);
    });
 }
+
+function onLoadMore(){
+  imagesApiService.getImages()
+  .then(dataProcessing)
+  .catch(error=>{
+      console.log(error);
+  });
+  
 }
 function dataProcessing(response){
     if (response.data.totalHits === 0) {
         return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     }
-    if (response.data.hits.length === 0) {
+    if (response.data.hits.length < 40) {
+      loadMoreBtn.disabled = true;
        return  Notify.warning(`We're sorry, but you've reached the end of search results.`);
     }
+      
+    console.log(imagesApiService.getPage());
+    console.log(response.data.hits.length)
+    loadMoreBtn.disabled = false;
     markup(response.data.hits);
     galleryLightBox.refresh();
     return Notify.info(`Hooray! We found ${response.data.totalHits} images.`);
+    
 }
 function markup(photoCards) {
     console.log(photoCards)
@@ -62,7 +81,7 @@ function markup(photoCards) {
     </div>`;
       })
       .join('');
-      return gallery.insertAdjacentHTML('afterbegin', markupInfo);
+      return gallery.insertAdjacentHTML('beforeend', markupInfo);
   }
 
   
